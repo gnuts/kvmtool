5edb5c2b07a8e10da6d87eb792d3b022a4f477ae
Matthias Nuessle <matthias.nuessle@mawoh.de>
2013-05-03 16:08:04 +0200

    * started debianizing

---
+++ debian/kvmtool-0.default.ex
@@ -0,0 +1,10 @@
+# Defaults for kvmtool-0 initscript
+# sourced by /etc/init.d/kvmtool-0
+# installed at /etc/default/kvmtool-0 by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(kvmtool-0):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="kvmtool-0" command="/usr/bin/kvmtool-0"
